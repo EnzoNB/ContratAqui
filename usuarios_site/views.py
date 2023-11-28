22a2bdf12d5fadcc423f72bcdb96518095407100
EnzoNB <89597352+EnzoNB@users.noreply.github.com>
2023-11-27 23:32:36 -0300

    Add db and Usuarios_site

--- usuarios_site/views.py
+++ usuarios_site/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from django.views import generic
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.views import LoginView
+from .forms import RegisterUserForm, CustomAuthenticationForm
 from django.urls import reverse_lazy
 
 class UserRegisterView(generic.CreateView):
-    form_class = UserCreationForm
+    form_class = RegisterUserForm
     template_name = "registration/register.html"
     success_url = reverse_lazy('login')
+
+class UserLoginView(LoginView):
+    form_class = CustomAuthenticationForm
+    template_name = "registration/login.html"
