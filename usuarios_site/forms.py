0d30fa88cade56428794afec9f8e7ba0925b89ba
EnzoNB <89597352+EnzoNB@users.noreply.github.com>
2023-11-28 10:16:10 -0300

    Atualização Site 2

--- usuarios_site/forms.py
+++ usuarios_site/forms.py
@@ -4,8 +4,8 @@ from django.contrib.auth.models import User
 
 class RegisterUserForm(UserCreationForm):
     email = forms.EmailField(label='Email')
-    primeiro_nome = forms.CharField(max_length=50, label='Primeiro nome')
-    ultimo_nome = forms.CharField(max_length=50, label='Último nome')
+    first_name = forms.CharField(max_length=50, label='Primeiro nome')  # Altere 'primeiro_nome' para 'first_name'
+    last_name = forms.CharField(max_length=50, label='Último nome')  # Altere 'ultimo_nome' para 'last_name'
     cpf = forms.CharField(max_length=14, label='CPF')
     cidade = forms.CharField(max_length=100, label='Cidade')
     username = forms.CharField(max_length=100, label='Nome de Login')
@@ -14,7 +14,8 @@ class RegisterUserForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields = ['username', 'primeiro_nome', 'ultimo_nome', 'email', 'cpf', 'cidade', 'password1', 'password2']
+        fields = ['username', 'first_name', 'last_name', 'email', 'cpf', 'cidade', 'password1', 'password2']
+
 
 class CustomAuthenticationForm(AuthenticationForm):
     username = forms.CharField(label='Nome de Login')
