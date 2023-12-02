1ed77d6deb3d34e06cd4d48769f046cb8928946f
Enzo <enzobarrese@usp.br>
2023-12-02 10:07:55 -0300

    Romever Prints

--- site_final/context_processors.py
+++ site_final/context_processors.py
@@ -3,10 +3,8 @@ from .models import Categoria
 
 def categorias(request):
     categorias = Categoria.objects.all()
-    print(categorias)  # Adicione esta linha
     cat=Categoria.objects.all()
     choices_dynamic = []
     for choice in cat:
         choices_dynamic.append(choice)
-    print(choices_dynamic)
     return {'list_types': choices_dynamic}
